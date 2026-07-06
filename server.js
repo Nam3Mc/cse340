048f2b1e9cce8dee0d4d1a0bdd0ce40ac858a00c
nam3mc <moralesdreiser5@gmail.com>
2026-07-06 11:03:16 -0400

    completing activity with ejs and express server

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production'
+const PORT = process.env.PORT || 3000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'src/views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    const title = 'HOME'
+    res.render('home', {title})
+});
+
+app.get('/organizations', (req, res) => {
+    const title = 'ORGANIZATION'
+    res.render('organizations', {title})
+});
+
+app.get('/projects', (req, res) => {
+    const title = 'PROJECTS'
+    res.render('projects', {title})
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`)
+      console.log(`Environment: ${NODE_ENV} and a change`)
+})
